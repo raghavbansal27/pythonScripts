9e5d0c9e4a5814d4d45c617d3c111415e55e8e4c
ani339677 <75009931+ani339677@users.noreply.github.com>
2020-12-07 13:11:25 +0530

    Update in the script.py and addition of folders

--- Script.py
+++ Script.py
@@ -1,14 +1,24 @@
 import os
 from datetime import date
-curr_dir="C:/Users/abv93/OneDrive/Documents/GitHub/pythonScripts/"
+curr_dir="GNE/Mech/Academics/"
 i=1
-check_dir=os.path.join(curr_dir,str(i))
-while os.path.isdir(check_dir)==True:
-    i+=1
-    check_dir=os.path.join(curr_dir,str(i))
+
 heading=str(input("Enter the Notice Heading:- "))
-os.mkdir(check_dir)
-file=open("C:/Users/abv93/OneDrive/Documents/GitHub/pythonScripts/noticeX.md","a")
-file.write("## "+heading+"\nDate : "+str(date.today())+"  \nNotice Number : "+str(i)+"  \n")
+
+pre=''
+
+if os.path.isfile("GNE/Mech/Academics/noticeX.md"):
+    file=open("GNE/Mech/Academics/noticeX.md","r")
+    pre=file.read()
+    file.seek(0,0)
+    line=file.readlines()
+    i=int(line[2][16:])
+    file.close()
+    os.rename(os.path.join(curr_dir,str(i)),os.path.join(curr_dir,str(i+1)))
+    i+=1
+else:
+    os.mkdir(os.path.join(curr_dir,str(i)))
+file=open("GNE/Mech/Academics/noticeX.md","w")
+file.write("## "+heading+"\nDate : "+str(date.today())+"  \nNotice Number : "+str(i)+"\n"+pre)
 file.flush()
 file.close()
