d2e9689821dfa69a238cea813aefe44de0f46d97
Raghav Bansal <raghavbansal292@gmail.com>
2020-12-09 10:32:57 +0530

    Bug fixed, It will create new directories, if not found

--- Script.py
+++ Script.py
@@ -9,36 +9,53 @@ regex = re.compile(r'\d+')
 pre = ''
 heading=str(input("Enter the Notice Heading:- "))
 
-total_notices = []
-for file in os.listdir(curr_dir):
-    if file.endswith(".md"):
-    	total_notices.append(file)
-if len(total_notices) != 0:    	
-	str1 = "".join(total_notices)
 
-	file_number_str = regex.findall(str1)
-	file_number_list = list(map(int, file_number_str)) 
-	file_number = max(file_number_list)
-	md_path = "GNE/Mech/Academics/notice" + str(file_number) + ".md"
-	path = os.path.abspath(os.path.join(base_dir, md_path))
-	#print(path)
-	if os.path.isfile(path):
+# Check for the current directory
+if os.path.isdir(curr_dir):
+	total_notices = []
+	for file in os.listdir(curr_dir):
+		if file.endswith(".md"):
+			total_notices.append(file)
+	if len(total_notices) != 0:    	
+		str1 = "".join(total_notices)
 
-		new_md_path = "GNE/Mech/Academics/notice" + str(file_number + 1) + ".md"
-		new_path = os.path.abspath(os.path.join(base_dir, new_md_path))
-		#print(new_path)
-		file_number+=1
-		file=open(new_path,"w")
+		file_number_str = regex.findall(str1)
+		file_number_list = list(map(int, file_number_str)) 
+		file_number = max(file_number_list)
+		md_path = "GNE/Mech/Academics/notice" + str(file_number) + ".md"
+		path = os.path.abspath(os.path.join(base_dir, md_path))
+		#print(path)
+		if os.path.isfile(path):
+
+			new_md_path = "GNE/Mech/Academics/notice" + str(file_number + 1) + ".md"
+			new_path = os.path.abspath(os.path.join(base_dir, new_md_path))
+			#print(new_path)
+			file_number+=1
+			file=open(new_path,"w")
+			file.write("## " + heading + "\nDate : " + str(date.today()) + "  \nNotice Number : " + str(file_number) + "\n" + pre)
+			file.flush()
+			file.close()
+			print("Notice added successfully!")
+		else:
+			print("File not found")
+
+
+	# Create new file if no .md file exist
+	else:
+		file_number = 1
+		md_path = "GNE/Mech/Academics/notice" + str(file_number) + ".md"
+		path = os.path.abspath(os.path.join(base_dir, md_path))
+		#print(path)
+		file=open(path,"w")
 		file.write("## " + heading + "\nDate : " + str(date.today()) + "  \nNotice Number : " + str(file_number) + "\n" + pre)
 		file.flush()
 		file.close()
 		print("Notice added successfully!")
-	else:
-		print("File not found")
 
 
-# Create new file if no .md file exist
+# Create new directories if not found
 else:
+	os.makedirs(curr_dir)
 	file_number = 1
 	md_path = "GNE/Mech/Academics/notice" + str(file_number) + ".md"
 	path = os.path.abspath(os.path.join(base_dir, md_path))
@@ -48,4 +65,3 @@ else:
 	file.flush()
 	file.close()
 	print("Notice added successfully!")
-
